63e6f42a5541fa79bd03342b68ea3ca3c94b0afd
leonid1108 <99071306+leonid1108@users.noreply.github.com>
2022-03-05 11:28:12 +0400

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -3,7 +3,7 @@ import math
 with open('text.txt','r') as file:
     sequence = file.read()
 count, even_num, odd_num  = 0, 0, 0
-main_mass = list(map(int, sequence.split(" ")))
+main_mass = list(map(int, sequence.split()))
 
 # ПОИСК ВСЕХ ЧЁТНЫХ И НЕЧЁТНЫХ ЦИФР В ПОСЛЕДОВАТЕЛЬНОСТИ
 for i in range(len(sequence)):
@@ -42,8 +42,8 @@ count = 0
 simple_num, not_simple_num = 0, 0
 for i in range(0, len(main_mass)):
     k = 0
-    for j in range(2, (int(main_mass[i]) // 2) + 1):
-        if (int(main_mass[i])) % j == 0:
+    for j in range(2, (main_mass[i] // 2) + 1):
+        if main_mass[i] % j == 0:
             k += 1
     if k <= 0:
         count += 1
@@ -53,3 +53,4 @@ for i in range(0, len(main_mass)):
         not_simple_num += 1
 print("Простых чисел ==>", round(((simple_num * 100) / count), 1), "%")
 print("Непростых чисел ==>", round(((not_simple_num * 100) / count), 1), "%")
+
